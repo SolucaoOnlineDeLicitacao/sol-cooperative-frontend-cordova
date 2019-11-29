0e1787f4ff0a793387c6a105f9f8622cadce0496
CAR <car@sdr-339.cards.ba.gov.br>
2019-11-29 10:51:58 -0300

    remoção do app-release.apk e config.xml do versionamento

---
+++ hooks/copy_build_extra.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const fs = require('fs')
+// fs.copyFileSync("./build-extras.gradle", "./platforms/android/build-extras.gradle");
+// fs.copyFileSync("./google-services.json", "./platforms/android/app/google-services.json");
