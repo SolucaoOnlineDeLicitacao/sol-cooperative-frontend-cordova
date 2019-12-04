f464ed6c39f5bf70516b8d2a296508a4034a22d7
CAR <car@sdr-339.cards.ba.gov.br>
2019-12-04 11:36:31 -0300

    acrecentada deleção do app-release.apk

--- assign.sh
+++ assign.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+rm -f app-release.apk && 
 cd platforms/android/app/build/outputs/apk/release &&
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore ../../../../../../../../../apk-cooperative/android.keystore app-release-unsigned.apk br.gov.ba.car.cooperative &&
 zipalign -v 4 app-release-unsigned.apk ../../../../../../../app-release.apk
